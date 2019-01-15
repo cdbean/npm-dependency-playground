0598c1d5f6d7bf184c2f89a6050073620b1d9188
Chen, Dong(dchen1) <dchen1@paypal.com>
2019-01-15 06:23:58 -0800

    base setting

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import a from 'moduleA'
+import b from 'moduleB'
+import c from 'moduleC'
+console.log("testApp")
+console.log('using moduleA')
+a()
+console.log('using moduleB')
+b()
+console.log('using moduleC')
+c()
